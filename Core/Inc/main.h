44fd9f343b1b90f111fd4a54d86956cdcc22f55a
thuantm <minhthuan1091996@gmail.com>
2023-12-12 14:43:03 +0700

    app: format and import mx25 driver

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -1,21 +1,21 @@
 /* USER CODE BEGIN Header */
 /**
-  ******************************************************************************
-  * @file           : main.h
-  * @brief          : Header for main.c file.
-  *                   This file contains the common defines of the application.
-  ******************************************************************************
-  * @attention
-  *
-  * Copyright (c) 2023 STMicroelectronics.
-  * All rights reserved.
-  *
-  * This software is licensed under terms that can be found in the LICENSE file
-  * in the root directory of this software component.
-  * If no LICENSE file comes with this software, it is provided AS-IS.
-  *
-  ******************************************************************************
-  */
+ ******************************************************************************
+ * @file           : main.h
+ * @brief          : Header for main.c file.
+ *                   This file contains the common defines of the application.
+ ******************************************************************************
+ * @attention
+ *
+ * Copyright (c) 2023 STMicroelectronics.
+ * All rights reserved.
+ *
+ * This software is licensed under terms that can be found in the LICENSE file
+ * in the root directory of this software component.
+ * If no LICENSE file comes with this software, it is provided AS-IS.
+ *
+ ******************************************************************************
+ */
 /* USER CODE END Header */
 
 /* Define to prevent recursive inclusion -------------------------------------*/
@@ -23,54 +23,66 @@
 #define __MAIN_H
 
 #ifdef __cplusplus
-extern "C" {
+extern "C"
+{
 #endif
 
 /* Includes ------------------------------------------------------------------*/
 #include "stm32g0xx_hal.h"
 
-/* Private includes ----------------------------------------------------------*/
-/* USER CODE BEGIN Includes */
+    /* Private includes
+     * ----------------------------------------------------------*/
+    /* USER CODE BEGIN Includes */
 
-/* USER CODE END Includes */
+    /* USER CODE END Includes */
 
-/* Exported types ------------------------------------------------------------*/
-/* USER CODE BEGIN ET */
+    /* Exported types
+     * ------------------------------------------------------------*/
+    /* USER CODE BEGIN ET */
 
-/* USER CODE END ET */
+    /* USER CODE END ET */
 
-/* Exported constants --------------------------------------------------------*/
-/* USER CODE BEGIN EC */
+    /* Exported constants
+     * --------------------------------------------------------*/
+    /* USER CODE BEGIN EC */
 
-/* USER CODE END EC */
+    /* USER CODE END EC */
 
-/* Exported macro ------------------------------------------------------------*/
-/* USER CODE BEGIN EM */
+    /* Exported macro
+     * ------------------------------------------------------------*/
+    /* USER CODE BEGIN EM */
 
-/* USER CODE END EM */
+    /* USER CODE END EM */
 
-/* Exported functions prototypes ---------------------------------------------*/
-void Error_Handler(void);
+    /* Exported functions prototypes
+     * ---------------------------------------------*/
+    void Error_Handler(void);
 
 /* USER CODE BEGIN EFP */
 
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define MCO_Pin GPIO_PIN_0
-#define MCO_GPIO_Port GPIOF
-#define SPI1_NSS_Pin GPIO_PIN_10
+#define MCO_Pin            GPIO_PIN_0
+#define MCO_GPIO_Port      GPIOF
+#define SPI1_NSS_Pin       GPIO_PIN_10
 #define SPI1_NSS_GPIO_Port GPIOA
-#define TMS_Pin GPIO_PIN_13
-#define TMS_GPIO_Port GPIOA
-#define TCK_Pin GPIO_PIN_14
-#define TCK_GPIO_Port GPIOA
-#define SPI2_NSS_Pin GPIO_PIN_15
+#define TMS_Pin            GPIO_PIN_13
+#define TMS_GPIO_Port      GPIOA
+#define TCK_Pin            GPIO_PIN_14
+#define TCK_GPIO_Port      GPIOA
+#define SPI2_NSS_Pin       GPIO_PIN_15
 #define SPI2_NSS_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
-
-/* USER CODE END Private defines */
+#define FLASH_RESET_PORT       GPIOD
+#define FLASH_RESET_PIN        GPIO_PIN_10
+#define FLASH_WP_PORT          GPIOD
+#define FLASH_WP_PIN           GPIO_PIN_11
+#define SPI1_NSS_PIN_NUMBER    15
+#define FLASH_RESET_PIN_NUMBER 10
+#define FLASH_WP_PIN_NUMBER    11s
+    /* USER CODE END Private defines */
 
 #ifdef __cplusplus
 }
