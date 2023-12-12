4d02b88017d39369ff31cb433597fcad6a81fff0
thuantm <minhthuan1091996@gmail.com>
2023-12-12 15:15:17 +0700

    driver: re-direct printf to UART1

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -62,7 +62,20 @@ void SystemClock_Config(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+#define PUTCHAR_PROTOTYPE int __io_putchar(int ch)
+/**
+  * @brief  Retargets the C library printf function to the USART.
+  * @param  None
+  * @retval None
+  */
+PUTCHAR_PROTOTYPE
+{
+  /* Place your implementation of fputc here */
+  /* e.g. write a character to the USART1 and Loop until the end of transmission */
+  HAL_UART_Transmit(&huart1, (uint8_t *)&ch, 1, 0xFFFF);
 
+  return ch;
+}
 /* USER CODE END 0 */
 
 /**
